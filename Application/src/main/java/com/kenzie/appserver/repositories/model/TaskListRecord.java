767e991d2e6d49c834e3ffc8c3d581d3208dd258
brandistaats <brandi.staats@mykenzie.snhu.edu>
2023-12-10 13:49:49 -0500

    Took User out of TaskListRecord and removed what I had in TaskList since Omer implemented it on his end

--- Application/src/main/java/com/kenzie/appserver/repositories/model/TaskListRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/TaskListRecord.java
@@ -3,12 +3,10 @@ package com.kenzie.appserver.repositories.model;
 public class TaskListRecord {
     public String userId;
     public String taskListName;
-    private User user;
 
-    public TaskList(String userId, String taskListName, User user) {
+    public TaskList(String userId, String taskListName) {
         this.userId = userId;
         this.taskListName = taskListName;
-        this.user = user;
     }
 
     public String getUserId() {
