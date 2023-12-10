daf81a1208857fe5147f635f82209ba998dc95dd
brandistaats <brandi.staats@mykenzie.snhu.edu>
2023-12-10 13:51:48 -0500

    removed my implementation because Omer implemented TaskList

--- Application/src/main/java/com/kenzie/appserver/service/model/TaskList.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/TaskList.java
@@ -1,31 +1,4 @@
 package com.kenzie.appserver.service.model;
 
 public class TaskList {
-    public String userId;
-    public String taskListName;
-    private User user;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getTaskListName() {
-        return taskListName;
-    }
-
-    public void setTaskListName(String taskListName) {
-        this.taskListName = taskListName;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
