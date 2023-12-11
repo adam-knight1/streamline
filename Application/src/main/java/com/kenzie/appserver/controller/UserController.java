be3c0141c4333f46128f30a7f224931c43aacf42
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-12-11 14:53:32 -0500

    Added userNotFoundException

--- Application/src/main/java/com/kenzie/appserver/controller/UserController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/UserController.java
@@ -1,4 +1,77 @@
 package com.kenzie.appserver.controller;
 
+import com.kenzie.appserver.UserNotFoundException;
+import com.kenzie.appserver.controller.model.UserCreateRequest;
+import com.kenzie.appserver.controller.model.UserResponse;
+import com.kenzie.appserver.service.UserService;
+import com.kenzie.appserver.service.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable("userId") String userId) {
+        System.out.println("Received request to find user with userId: " + userId);
+        User user = userService.findByUserId(userId);
+        if (user == null) {
+            throw new UserNotFoundException("User not found"); // added this custom exception -adam
+        }
+        return ResponseEntity.ok(userToResponse(user));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createNewUser(@RequestBody UserCreateRequest userCreateRequest) {
+        User user = new User(
+                userCreateRequest.getUsername(),
+                userCreateRequest.getPassword(),
+                userCreateRequest.getEmail()
+        );
+
+        try {
+            userService.createNewUser(user);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return  ResponseEntity.ok(userToResponse(user));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable("userId") String userId, @RequestBody User updatedUserInfo) {
+        Optional<User> optionalUpdatedUser = userService.updateUser(userId, updatedUserInfo);
+        return optionalUpdatedUser.map(user -> ResponseEntity.ok(userToResponse(user)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+        if(userService.deleteUser(userId)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private UserResponse userToResponse(User user) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUserId(user.getUserId());
+        userResponse.setUsername(user.getUsername());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        return userResponse;
+    }
+
 }
--- Application/src/main/java/com/kenzie/appserver/controller/model/UserCreateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/UserCreateRequest.java
@@ -1,4 +1,4 @@
 package com.kenzie.appserver.controller.model;
 
-public class CreateUserRequest {
+public class UserCreateRequest {
 }
