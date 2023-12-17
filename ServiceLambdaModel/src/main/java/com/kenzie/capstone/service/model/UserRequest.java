47bb94501121d706f5da6c9fd5f874b8cbd581ec
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-12-17 18:37:44 -0500

    Added UserRequest and UserResponse in LambdaModel

---
+++ ServiceLambdaModel/src/main/java/com/kenzie/capstone/service/model/UserRequest.java
@@ -0,0 +1,40 @@
+package com.kenzie.capstone.service.model;
+
+public class UserRequest {
+    private String userId;
+    private String email;
+    private String password;
+    private String username;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
