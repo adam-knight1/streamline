1abe8f6ece83e199402a169962529166b0e89d8a
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-12-17 18:15:29 -0500

    Added CreateUser lambda and added handle request method

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaUserService.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaUserService.java
@@ -1,12 +1,39 @@
 package com.kenzie.capstone.service;
 
 import com.kenzie.capstone.service.dao.UserDao;
+import com.kenzie.capstone.service.model.UserRecord;
 
 public class LambdaUserService {
 
     private UserDao userDao;
+
     public LambdaUserService(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    public UserRecord findByUserId(String userId) {
+        return userDao.findUserById(userId);
+    }
+
+    public UserRecord createNewUser(UserRecord userRecord) {
+        try {
+            return userDao.createUser(userRecord);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public UserRecord updateUser(UserRecord userRecord) {
+        return userDao.updateUser(userRecord);
+    }
+
+    public boolean deleteUser(String userId) {
+        try {
+            userDao.deleteUser(userId);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
+
