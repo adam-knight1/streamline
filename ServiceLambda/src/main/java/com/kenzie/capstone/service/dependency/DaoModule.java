833691b951d13dbd3373217073048c433eb9d230
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-12-17 17:16:49 -0500

    Added UserDao created provideUserDao method in DaoModule

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
@@ -3,6 +3,7 @@ package com.kenzie.capstone.service.dependency;
 
 import com.kenzie.capstone.service.dao.ExampleDao;
 import com.kenzie.capstone.service.dao.TaskDao;
+import com.kenzie.capstone.service.dao.UserDao;
 import com.kenzie.capstone.service.util.DynamoDbClientProvider;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -40,5 +41,13 @@ public class DaoModule {
     public TaskDao provideTaskDao(@Named("DynamoDBMapper") DynamoDBMapper mapper) {
         return new TaskDao(mapper);
     }
+
+    @Singleton
+    @Provides
+    @Named("UserDao")
+    public UserDao provideUserDao(@Named("DynamoDBMapper") DynamoDBMapper mapper) {
+        return new UserDao(mapper);
+    }
+
 
 }
