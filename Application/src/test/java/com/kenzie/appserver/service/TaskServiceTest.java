6b9cc3390df490ee084329140f85d36d1844aff7
Remai Bowens <remaibowens1@gmail.com>
2023-12-30 16:31:50 -0500

    started TaskService test

--- Application/src/test/java/com/kenzie/appserver/service/TaskServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/TaskServiceTest.java
@@ -1,8 +1,13 @@
 package com.kenzie.appserver.service;
 
+import com.kenzie.appserver.repositories.TaskRepository;
+import com.kenzie.appserver.repositories.model.TaskRecord;
 import com.kenzie.appserver.service.model.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,16 +18,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class TaskServiceTest {
 
-    @Autowired
+    @Mock
+    private TaskRepository taskRepository;
     private TaskService taskService;
 
+    @Captor
+    private ArgumentCaptor<TaskRecord> taskRecordCaptor;
+
     @Test
-    public void testAddTask(){ //need to finish
+    public void AddTask_Sucessful(){
         //GIVEN
         Task task = new Task("Example Task", "Description of task", true);
 
         //WHEN
-        Task addedTask ;
+        Task addedTask = taskService.addTask(task);
 
         //THEN
 
