9b366cbc77755feed4196595ffcdf32cac4be561
Remai Bowens <remaibowens1@gmail.com>
2023-12-29 22:12:18 -0500

    fixed Task repository

--- Application/src/main/java/com/kenzie/appserver/repositories/TaskRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/TaskRepository.java
@@ -2,11 +2,20 @@ package com.kenzie.appserver.repositories;
 
 import com.kenzie.appserver.repositories.model.ExampleRecord;
 
+import com.kenzie.appserver.repositories.model.TaskRecord;
 import com.kenzie.appserver.service.model.Task;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-/*@EnableScan
-public interface TaskRepository extends CrudRepository<Task, String> {
+import java.util.List;
 
-}*/
+@EnableScan
+public interface TaskRepository extends CrudRepository<TaskRecord, String> {
+
+    //find tasks by userId
+    List<TaskRecord> findUserById (String userId);
+
+    //delete tasks by userid
+    void deleteByUserId(String userId);
+
+}
