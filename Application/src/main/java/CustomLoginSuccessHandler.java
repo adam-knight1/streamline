9e160de0d9270aaf9be658850bcc169556d5dc9e
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-01-01 22:26:23 -0500

    Added names to login.html for security recognition

--- Application/src/main/java/CustomLoginSuccessHandler.java
+++ Application/src/main/java/CustomLoginSuccessHandler.java
@@ -1,2 +1,20 @@
-public class CustomLoginSuccessHandler {
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+
+@Component
+public class CustomLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException, IOException {
+        
+        response.sendRedirect("/userHomePage.html");
+    }
 }
+
