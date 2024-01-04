2b4a0f59efddfe46bef3b63f13d4ce89e5951603
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-01-03 19:04:43 -0500

    Commented out spring security stuff to troubleshoot

--- Application/src/main/java/com/kenzie/appserver/DynamoDBUserDetailsService.java
+++ Application/src/main/java/com/kenzie/appserver/DynamoDBUserDetailsService.java
@@ -2,6 +2,8 @@ package com.kenzie.appserver;
 
 import com.kenzie.appserver.repositories.UserRepository;
 import com.kenzie.appserver.repositories.model.UserRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,16 +18,24 @@ public class DynamoDBUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(DynamoDBUserDetailsService.class);
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         Optional<UserRecord> optionalUserRecord = userRepository.findByUsername(username);
 
+        logger.info("Loading user by username: {}", username);
+
+
         if (optionalUserRecord.isPresent()) {
             UserRecord userRecord = optionalUserRecord.get();
-
+            logger.info("User found: {}", userRecord);
             return new CustomUserDetails(userRecord);
         } else {
+            logger.error("User not found: {}", username);
+            //adding log statements to debug login process
             throw new UsernameNotFoundException("User not found: " + username);
         }
     }
