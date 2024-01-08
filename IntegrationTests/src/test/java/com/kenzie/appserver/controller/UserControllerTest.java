634aee94759b881d75c5de6e0d8d2de355830ece
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-01-08 17:18:00 -0500

    Added comments for code readability, added class UserControllerTest

---
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/UserControllerTest.java
@@ -0,0 +1,35 @@
+package com.kenzie.appserver.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kenzie.appserver.IntegrationTest;
+import com.kenzie.appserver.service.UserService;
+import net.andreinc.mockneat.MockNeat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@IntegrationTest
+class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserService userService;
+
+    private final MockNeat mockNeat = MockNeat.threadLocal();
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void getUserByUserId_Exists() throws Exception {}
+}
