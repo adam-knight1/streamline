bc36868aba6c429dec258e7fe2fab989156908e7
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-01-14 07:27:25 -0500

    Updating task variables

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -7,10 +7,10 @@
 #If the casing is different you will have issues.
 #for example if the username was tHisIS-bOb this would read:
 #export CAPSTONE_REPO_NAME=ata-capstone-project-tHisIS-bOb
-export CAPSTONE_REPO_NAME=ata-capstone-project-remaibowens
+export CAPSTONE_REPO_NAME=streamline
 
 # Fill out the following value with the group name in all lowercase and no spaces or special characters.
-export GITHUB_GROUP_NAME=streamteam
+export GITHUB_GROUP_NAME=capstoneadam
 
 # Step Two - configure your shell to always have these variables.
 # For OSX / Linux
