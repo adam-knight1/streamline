8fb9e0a0e6d4d2f5f8d6904928110d4ac2ac499b
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-01-24 14:07:34 -0500

    spring security is fully implemented and working, fetching data and logging in successfully.

--- Application/src/main/java/com/kenzie/appserver/config/WebSecurityConfig.java
+++ Application/src/main/java/com/kenzie/appserver/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/css/**", "/api/**" , "/pages/**", "/user/create", "/user/**", "/index", "/swagger-ui/index.html").permitAll()
+                .antMatchers("/css/**", "/error" , "/api/**" , "/pages/**", "/user/create", "/user/**", "/index", "/swagger-ui/index.html").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
